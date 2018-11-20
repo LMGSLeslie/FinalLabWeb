1f2a77e0a445ee259a2a5d2d5c0cd63a8a9a7a91
Jorge <jorgejimenezgar@hotmail.com>
2018-11-20 12:24:28 -0600

    Agregué archivos de Routes y Controller de la BD

---
+++ src/videojuegosRoutes.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = function(app) {
+  var collections = require('./videojuegosController');
+  app.route('/plataforma/:idPlataforma')
+  .get(collections.obtener_plataforma_id);
+  app.route('/plataforma/busqueda/:palabraClave')
+  .get(collections.obtener_plataformas_palabra);
+  app.route('/plataforma/:idPlataforma/juegos')
+  .get(collections.obtener_juegos_plataforma);
+  app.route('/juegos/busqueda/:palabraClave')
+  .get(collections.obtener_juegos_palabra);
+  app.route('/plataforma/:idPlataforma/juego/:idJuego')
+  .get(collections.obtener_juego_plataforma);
+  app.route('/blogs')
+  .get(collections.obtener_blogs);
+};
